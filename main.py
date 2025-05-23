06743a7c8826a5aac7c5c03da3390c2d6dda1bdb
Goldfish728 <2973202329@qq.com>
2025-05-23 19:01:13 +0800

    init single agent quic

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 from app.logger import logger
 from app.utils.visualize_record import save_record_to_json, generate_visualization_html
 
+from app.config import PROJECT_ROOT
+
 async def run_agent(task_name: str, prompt: str) -> None:
     """
     执行特定任务的智能体的主入口点。
@@ -94,15 +96,22 @@ if __name__ == "__main__":
     # for task, prompt in zip(task_name, prompt):
     #     asyncio.run(run_agent(task, prompt))
 
-    prompt = """quic协议是一个传输层协议，aioquic是该协议的python实现。
+    work_dir = f"{PROJECT_ROOT}/quic_project"
+    
+    prompt = f"""quic协议是一个传输层协议，aioquic是该协议的python实现。
 
-    项目文件路径是/app/quic_project，内有两个文件夹/client和/ai_generate。
-    client文件夹内含两个文件，quic客户端是文件quic_file_client.py，它默认加载Quic_default_config.json文件作为配置参数。
-    你生成的文件请放在/ai_generate文件夹下。
+    项目文件路径是{work_dir}，
+    quic客户端是文件quic_file_client.py，它默认加载Quic_default_config.json文件作为配置参数。
+    你生成的文件也需放在{work_dir}文件夹下。
 
-    测试任务是quic客户端从quic服务器下载文件,文件名为testfile_10M，quic服务器运行在118.89.124.177:4433。
-    现在模拟到服务器单向时延100ms，丢包率10%，你需要根据网络和应用特点，确定该场景下的配置文件，使得下载速度优于默认参数。
-    请给出你的推理过程，并给出最终的配置参数。请注意，禁止阅读aioquic源码。
+    测试任务是quic客户端从quic服务器下载文件,文件名为testfile_1M，quic服务器运行在118.89.124.177:4433。测试时模拟时延100ms，丢包率10%。
+    你需要请根据网络和应用特点，确定该场景下的配置文件（使用json_saver保存），并且通过测试证明下载性能优于默认配置文件。
+    请给出你的推理过程，并给出最终的配置文件。请注意，禁止阅读aioquic源码。
 """
-    asyncio.run(run_agent("quic_download", prompt))
+
+    asyncio.run(run_agent("quic_mcp_test08", prompt))
+
+#优化quic_test_mcp,不再使用子进程的方式
+
+
 
